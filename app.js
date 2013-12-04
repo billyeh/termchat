257f84647c1dbca08bc56b668c40522e54c3811a
= <billwyeh@yahoo.com>
2013-12-04 14:17:49 -0800

    rudimentary chat working like a boss

--- app.js
+++ app.js
@@ -8,34 +8,26 @@ app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
 });
 
-room_dict = {};
-user_dict = {};
+// rooms[room_name] = [user1, user2, ...]
+rooms = {};
+// users[username] = socket
+users = {};
+// requests['chat'][requester_username] = requested_username
+requests = {'chat': {}, 'video': {}};
 
 io.sockets.on('connection', function (socket) {
-  socket.on('join', function (data) {
-    socket.join(data.room);
-    if (data.room in room_dict) {
-      room_dict[data.room].push(data.user);
-    } else {
-      room_dict[data.room] = [data.user];
+  socket.on('check_name', function(data) {
+    console.log('Validation request for ' + data.name + ': ' + (data.name in users).toString());
+    if (data.name in users) {
+      socket.emit('validation', {valid: 'invalid', user: data.name});
     }
-    console.log('Request to join room ' + data.room + ' received from ' + data.user);
-    user_dict[data.user] = socket;
-    io.sockets.in(data.room).emit('new_user', {user: data.user});
+    else {
+      socket.emit('validation', {valid: 'valid', user: data.name});
+    }
   });
 
-  socket.on('leave', function (data) {
-    socket.leave(data.room);
-    if (data.room in room_dict) {
-      var index = room_dict[data.room].indexOf(data.user);
-      if (index > -1) {
-        room_dict[data.room].splice(index, 1);
-      }
-    }
-    console.log('Request to leave room ' + data.room + ' received from ' + data.user);
-    delete user_dict[data.user];
-    io.sockets.in(data.room).emit('leave_user', {user: data.user});
-  });
+  socket.on('join', join);
+  socket.on('leave', leave);
 
   socket.on('message', function(data) {
     console.log('Message ' + data.msg + ' received from ' + data.user);
@@ -44,14 +36,86 @@ io.sockets.on('connection', function (socket) {
 
   socket.on('get_users', function(data) {
     console.log('User list request for room ' + data.room + ' received');
-    socket.emit('user_list', {users: room_dict[data.room]});
+    socket.emit('user_list', {users: rooms[data.room]});
+  });
+
+  socket.on('chat_request', function(data) {
+    console.log('Request to chat ' + data.other + ' from ' + data.user);
+    if (!users[data.other]) {
+      socket.emit('no_user', {user: data.other});
+    }
+    else {
+      requests['chat'][data.user] = data.other;
+      users[data.other].emit('chat?', {user: data.user});
+    }
   });
 
   socket.on('video_request', function(data) {
     console.log('Request to video chat ' + data.other + ' from ' + data.user);
+    if (!users[data.other]) {
+      socket.emit('no_user', {user: data.other});
+    }
+    else {
+      requests['video'][data.user] = data.other;
+      users[data.other].emit('video?', {user: data.user});
+    }
   });
 
-  socket.on('chat_request', function(data) {
-    console.log('Request to video chat ' + data.other + ' from ' + data.user);
+  socket.on('accept', function(data) {
+    if (!(data.other in users)) {
+      socket.emit('no_user', {user: data.other});
+    }
+    else if (data.other in requests['chat']) {
+      startChat(data.user, data.other);
+    }
+    else if (data.other in requests['video']) {
+      startVideo(data.user, data.other);
+    }
+    else {
+      socket.emit('no_request', {user: data.other});
+    }
   });
 });
+
+function join(data) {
+  this.join(data.room);
+  if (data.room in rooms) {
+    rooms[data.room].push(data.user);
+  }
+  else {
+    rooms[data.room] = [data.user];
+  }
+  console.log('Request to join room ' + data.room + ' received from ' + data.user);
+  users[data.user] = this;
+  io.sockets.in(data.room).emit('new_user', {user: data.user});
+}
+
+function leave(data) {
+  this.leave(data.room);
+  if (data.room in rooms) {
+    var index = rooms[data.room].indexOf(data.user);
+    if (index > -1) {
+      rooms[data.room].splice(index, 1);
+    }
+  }
+  console.log('Request to leave room ' + data.room + ' received from ' + data.user);
+  delete users[data.user];
+  delete requests['chat'][data.user];
+  delete requests['video'][data.user];
+  io.sockets.in(data.room).emit('leave_user', {user: data.user});
+}
+
+function startChat(u1, u2) {
+  console.log('Starting chat for ' + u1 + ' and ' + u2);
+  var u1Room = Object.keys(io.sockets.manager.roomClients[users[u1].id])[0];
+  var u2Room = Object.keys(io.sockets.manager.roomClients[users[u2].id])[0];
+  delete requests['chat'][u2];
+  users[u1].join({room: u1 + u2, user: u1});
+  users[u2].join({room: u1 + u2, user: u2});
+  users[u1].emit('new_room', {room: u1 + u2, user: u2});
+  users[u2].emit('new_room', {room: u1 + u2, user: u1});
+}
+
+function startVideo(u1, u2) {
+  console.log('Starting video for ' + u1 + ' and ' + u2); 
+}
--- client.js
+++ client.js
@@ -6,6 +6,7 @@ var io = require('socket.io-client')
 
 var socket
   , username
+  , other
   , room = ''
   , win
   , box
@@ -14,6 +15,24 @@ var socket
 main();
 
 function main() {
+  getName();
+  socket = io.connect('http://localhost:8000/');
+  socket.on('connect', function(data) {
+    socket.emit('connection');
+  });
+  socket.on('validation', function(data) {
+    if (data.valid === 'valid') {
+      listen();
+      getUserInput();
+    }
+    else {
+      console.log('Sorry, that name is not available.');
+      getName();
+    }
+  });
+}
+
+function getName() {
   prompt.message = "";
   prompt.delimiter = "";
   prompt.start();
@@ -28,26 +47,12 @@ function main() {
 }
 
 function checkName(err, result) {
-  if (err) {
-    console.log('Error encountered logging you in');
-  }
-  else {
-    username = result.name;
-    joinRoom();
-  }
+  username = result.name;
+  socket.emit('check_name', {name: result.name});
 }
 
-function joinRoom() {
-  socket = io.connect('http://localhost:8000/');
-  socket.on('connect', function(data) {
-    socket.emit('connection');
-    socket.emit('join', {room: room, user: username});
-    listen(socket);
-    getUserInput();
-  });
-}
-
-function listen(socket) {
+function listen() {
+  socket.emit('join', {room: room, user: username});
   socket.on('user_list', function(data) {
     writeMessage('Current users: ' + JSON.stringify(data.users) + '\n');
   });
@@ -59,6 +64,27 @@ function listen(socket) {
   });
   socket.on('new_message', function(data) {
     writeMessage(data.user + ': ' + data.msg + '\n');
+  });
+  socket.on('chat?', function(data) {
+    other = data.user;
+    writeMessage(data.user + ' would like to chat with you.\n' +
+                 'Type /y to accept their request.\n');
+  });
+  socket.on('video?', function(data) {
+    other = data.user;
+    writeMessage(data.user + ' would like to video chat with you.\n' +
+                 'Type /y to accept their request.\n');
+  });
+  socket.on('no_user', function(data) {
+    writeMessage('Tried chatting with ' + data.user + ', but they are not on termchat.\n' + 
+                 'Type /users to see who\'s on termchat right now.\n');
+  });
+  socket.on('no_request', function(data) {
+    writeMessage('There are no chat requests to accept right now.\n');
+  });
+  socket.on('new_room', function(data) {
+    room = data.room;
+    writeMessage('You\'ve entered a chat room with ' + data.user + '!\n');
   });
 }
 
@@ -113,17 +139,12 @@ function writeMessage(contents, color) {
 
 function sendMessage(value) {
   var words = value.split(' ');
-  value = words.slice(1, words.length).join(' ');
-  if (!(!!value)) {
-    return;
-  }
+  if (value[0] === '>') { value = words.slice(1, words.length).join(' '); }
+  if (!(!!value)) { return; }
   if (value.substring(0, 1) === '/' && value.substring(1, 2) !== '/') {
     var command = value.split(' ')[0].substring(1, value.split(' ')[0].length);
     var args = value.split(' ').slice(1, value.split(' ').length).join(' ');
-    var helpMessage = parseHelp(command, args);
-    if (helpMessage) {
-      writeMessage(helpMessage);
-    }
+    writeMessage(parseHelp(command, args));
   }
   else if (value.substring(0, 1) === '/') {
     socket.emit('message', {room: room, user: username, msg: value.substring(1, value.length)});
@@ -135,30 +156,37 @@ function sendMessage(value) {
 
 function parseHelp(command, args) {
   switch(command) {
-    case 'help': 
+    case 'help':
       return 'To send a message starting with \'/\', ' + 
               'type \'//\'.\nA list of valid commands:\n' +
               '/help: display this help message\n' +
               '/users: display a list of users in the room\n' +
-              '/chat {username}: enter a private chatroom with someone\n' +
-              '/video {username}: enter a video call with someone\n';
+              '/chat {username}: enter a chatroom with someone\n' +
+              '/video {username}: enter a video call with someone\n' + 
+              '/y: accept the last chat or video request you received\n';
     case 'users':
       socket.emit('get_users', {room: room});
-      return false;
+      return '';
     case 'chat':
       if (!args.split(' ')[0]) {
         return 'Enter a username to chat with after the /chat command.\n';
       }
       socket.emit('chat_request', {user: username, other: args.split(' ')[0]});
-      return 'Chatting ' + args.split(' ')[0] + '...\n';
+      return 'Requesting to chat with ' + args.split(' ')[0] + '...\n';
     case 'video':
       if (!args.split(' ')[0]) {
         return 'Enter a username to video chat with after the /video command.\n';
       }
       socket.emit('video_request', {user: username, other: args.split(' ')[0]});
-      return 'Calling ' + args.split(' ')[0] + '...\n';
+      return 'Requesting video call with ' + args.split(' ')[0] + '...\n';
+    case 'y':
+      if (!(!!other)) {
+        return 'There is no request you can accept right now.\n';
+      }
+      socket.emit('accept', {user: username, other: other});
+      return '';
     default:
-      return 'Command \'' + command + '\' not recognized. Enter /help for commands.\n'; 
+      return 'Command \'' + command + '\' not recognized. Enter /help for commands.\n';
   }
 }
 
